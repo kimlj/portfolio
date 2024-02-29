774c85e4c6bf5ac534686f1ad54c9e5aadf202c3
kimlj <kljulongbayan@gmail.com>
2024-02-29 21:40:06 +0800

    added navbar component, navbar css

---
+++ src/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {Link} from "react-router-dom"
+import './navbar.css'
+// ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+
+function Navbar (){
+    return (
+        <>
+        <nav>
+
+        </nav>
+        </>
+    )
+}
