0efb09e967d83876830576caa1d661141101bbd9
kimlj <kljulongbayan@gmail.com>
2024-03-04 22:30:15 +0800

    added navbar li sharedLinkStyles

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -8,20 +8,34 @@ import Academics from "./academics"
 // ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
 
 function Navbar() {
+    // Custom class for shared link styles
+    const sharedLinkStyles = "text-black px-8 hover:text-gray-300";
+
     return (
-        <nav className="navbar">
-            <ul>
+        <nav className="bg-white">
+            <ul className="flex justify-between px-4 py-2">
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/" className={sharedLinkStyles}>Home</Link>
                 </li>
                 <li>
-                    <Link to="/academics">Academics</Link>
+                    <Link to="/academics" className={sharedLinkStyles}>Academics</Link>
+                </li>
+                <li>
+                    <Link to="/certifications" className={sharedLinkStyles}>Certifications</Link>
+                </li>
+                <li>
+                    <Link to="/workExperience" className={sharedLinkStyles}>Work Experience</Link>
+                </li>
+                <li>
+                    <Link to="/About" className={sharedLinkStyles}>About</Link>
                 </li>
                 {/* Add other links as needed */}
             </ul>
         </nav>
     );
 }
+
+
 
 
 export default Navbar
