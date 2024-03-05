a26a54762f148a87c968a5f65a4718942ac72a61
kimlj <kljulongbayan@gmail.com>
2024-03-05 15:48:58 +0800

    center aligned nav ul

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -31,9 +31,10 @@ function Navbar() {
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
             </svg>
+
         </button >
         <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
-        <ul className="font-medium flex flex-col mt-4 bg-white p-7 rounded-3xl md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul className="align-center justify-center font-medium flex flex-col mt-4 bg-white p-7 rounded-3xl md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li className="flex-initial">
                     <Link to="/" className={sharedLinkStyles}>Home</Link>
                 </li>
