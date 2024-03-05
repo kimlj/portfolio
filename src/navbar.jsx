64d63b10a5a663a6bd0fa3b0c1ba1ecfceff6f53
kimlj <kljulongbayan@gmail.com>
2024-03-05 12:27:06 +0800

    added responsive nav with burger menu

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -4,36 +4,57 @@ import {Link} from "react-router-dom"
 import './navbar.css'
 import Home from "./home"
 import Academics from "./academics"
+import { useState } from "react"
 
 // ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
 
 function Navbar() {
+    // state when hamburger menu was clicked and opened
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     // Custom class for shared link styles
-    const sharedLinkStyles = "text-black px-8 hover:text-gray-300";
+    const sharedLinkStyles = "block py-2 px-3 text-black rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500";
 
     return (
-        <nav className="bg-white">
-            <ul className="flex justify-between px-4 py-2">
-                <li>
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <button onClick={toggleMenu}
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded={isMenuOpen ? "true" : "false"}>
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+        </button >
+        <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li className="flex-initial">
                     <Link to="/" className={sharedLinkStyles}>Home</Link>
                 </li>
-                <li>
+                <li className="flex-initial">
                     <Link to="/academics" className={sharedLinkStyles}>Academics</Link>
                 </li>
-                <li>
+                <li className="flex-initial">
                     <Link to="/certifications" className={sharedLinkStyles}>Certifications</Link>
                 </li>
-                <li>
+                <li className="flex-initial">
                     <Link to="/workExperience" className={sharedLinkStyles}>Work Experience</Link>
                 </li>
-                <li>
+                <li className="flex-initial">
                     <Link to="/About" className={sharedLinkStyles}>About</Link>
                 </li>
                 {/* Add other links as needed */}
             </ul>
+        </div>
+            
         </nav>
     );
 }
+
 
 
 
