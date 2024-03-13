5708ede31d82f032f004deee87a9f4e7f7f94de1
kimlj <kljulongbayan@gmail.com>
2024-03-13 14:48:50 +0800

    hide hamburger icon initially using min width

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -13,6 +13,7 @@ function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
+        console.log(isMenuOpen)
     };
 
     // Custom class for shared link styles
@@ -24,22 +25,11 @@ function Navbar() {
 
     return (
         <nav className="">
-        {/* <button onClick={toggleMenu}
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded={isMenuOpen ? "true" : "false"}>
-            <span className="sr-only">Open main menu</span>
-            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-            </svg>
-        </button > */}
-        {/* show menu here */}
         <button className="hamburger-menu" onClick={toggleMenu}>
             <img src={hamburgerMenuPath} alt="" className="hamburger-icon"/>
         </button>
         <img className = "profile" src={profileImagePath} alt="" />
-        <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+        <div id="navbar-default" className = {`${isMenuOpen ? 'menu-visible' : ''}`}>
             <div className="ul-holder">
                 <ul className="nav-ul">
                     <li className="nav-ul-li">
